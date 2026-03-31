425a45bcd52d31dd277d6c2806f2e9d23ac2fd44
loiceakello <loicedoti@gmail.com>
2026-03-31 19:56:16 +0300

    Fix HymnViewSet to use correct serializer and expose fields

--- hymns/views.py
+++ hymns/views.py
@@ -4,14 +4,6 @@ from .models import Hymn
 from .serializers import HymnSerializer
 from django.shortcuts import render
 
-def hymn_list(request):
-    hymns = Hymn.objects.all().order_by("number")
-    return render(request, "hymns/hymn_list.html", {"hymns": hymns})
-
-def hymn_detail(request, hymn_id):
-    hymn = Hymn.objects.get(id=hymn_id)
-    return render(request, "hymns/hymn_detail.html", {"hymn": hymn})
-
 
 class HymnViewSet(viewsets.ModelViewSet):
     queryset = Hymn.objects.all()
